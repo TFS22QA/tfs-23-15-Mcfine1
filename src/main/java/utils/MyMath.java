f2230f3e383c9da1f4f05ba5c23e23e29b24c4f4
RomchikQW <rverendeea@gmail.com>
2023-10-12 19:51:21 +0300

    roma

--- src/main/java/utils/MyMath.java
+++ src/main/java/utils/MyMath.java
@@ -3,14 +3,8 @@ package utils;
 public class MyMath {
 
     public static int getDigitsSum(final int i) {
-        int src;
-        if (i < 0) {
-            src = i * -1;
-        } else {
-            src = i;
-        }
         int sum = 0;
-        final String numberAsString = String.valueOf(src);
+        final String numberAsString = String.valueOf(i);
         final String[] digitsAsString = numberAsString.split("");
         for (String stringDigit : digitsAsString) {
             sum += Integer.parseInt(stringDigit);
